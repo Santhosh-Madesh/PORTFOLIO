238db327f4529eeb29850dbe0d1b6722e4c9d100
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2025-11-03 09:25:11 +0530

    updated urls

--- DJANGOPORTFOLIO/PORTFOLIO/urls.py
+++ DJANGOPORTFOLIO/PORTFOLIO/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import home, LatestGitHubActivityAPIView, LatestGitHubProjectsAPIView
+from django.urls import path, re_path
+from .views import home, LatestGitHubActivityAPIView, LatestGitHubProjectsAPIView, FrontendAppView
 
 urlpatterns = [
     path('', home, name='home'),
     path('api/projects/', LatestGitHubProjectsAPIView.as_view(), name="projects"),
-    path('api/events/', LatestGitHubActivityAPIView.as_view(), name='activity')
+    path('api/events/', LatestGitHubActivityAPIView.as_view(), name='activity'),
+    re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),
 ]
