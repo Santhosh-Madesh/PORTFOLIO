170e174854423ee9e4c5f3ecea58fb5926a71a2a
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2025-11-03 09:25:37 +0530

    updated settings

--- DJANGOPORTFOLIO/DJANGOPORTFOLIO/settings.py
+++ DJANGOPORTFOLIO/DJANGOPORTFOLIO/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^@64c=gr*v4jv46-%ork*v62(q0kp21_7ks+@znzh62ysd+5_v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
